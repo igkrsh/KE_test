99d1b7e3789db911aa418889439cb0f281aa5df4
Igor Krasheninnikov <igor.krash@gmail.com>
2021-09-30 17:37:53 +0300

    + simple server impl
    Signed-off-by: Igor Krasheninnikov <igor.krash@gmail.com>

--- server.go
+++ server.go
@@ -15,6 +15,31 @@ type Service interface {
 	Process(ctx context.Context, batch Batch) error
 }
 
+type Server struct {
+	elemLimit uint64
+	timeLimit time.Duration
+	processed uint64
+}
+
+func SetLimits(serv Server, n uint64, p time.Duration) {
+	serv.elemLimit = n
+	serv.timeLimit = p
+	serv.processed = 0
+}
+
+func GetLimits(serv Server) (n uint64, p time.Duration) {
+	return serv.elemLimit, serv.timeLimit
+}
+
+func Process(serv Server, ctx context.Context, batch Batch) error {
+	if serv.processed < serv.elemLimit {
+		serv.processed ++
+	} else {
+		return ErrBlocked
+	}
+	return nil
+}
+
 // Batch is a batch of items.
 type Batch []Item
 
